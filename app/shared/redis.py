7dd55c8a7540446a1fbf391a617d59b9041724fb
Jishan <jishan@Jishans-MacBook-Pro.local>
2026-06-01 15:30:36 +1000

    add Redis ConfigMap and configure liveness/readiness probes

--- app/shared/redis.py
+++ app/shared/redis.py
@@ -7,7 +7,10 @@ from app.shared.circuit_breaker import CircuitBreaker
 
 RedisHostType = Literal["sync", "inference"]
 SYNC_REDIS_HOST = os.getenv("REDIS_SYNC_HOST", "localhost")
-SYNC_REDIS_PORT = int( os.getenv("REDIS_SYNC_PORT", "6379"))
+SYNC_REDIS_PORT = int(os.getenv("REDIS_SYNC_PORT", "6380"))
+
+REDIS_INFERENCE_HOST = os.getenv("REDIS_INFERENCE_HOST", "localhost")
+REDIS_INFERENCE_PORT = int(os.getenv("REDIS_INFERENCE_PORT", "6379"))
 
 sync_redis_client: Redis = redis.Redis(
     host=SYNC_REDIS_HOST,
@@ -20,8 +23,8 @@ sync_redis_client: Redis = redis.Redis(
 )
 
 inference_redis_client: Redis = redis.Redis(
-    host="redis_inference",
-    port=6379,
+    host=REDIS_INFERENCE_HOST,
+    port=REDIS_INFERENCE_PORT,
     decode_responses=True,
     socket_connect_timeout=0.2,  # connection timeout
     socket_timeout=0.2,
